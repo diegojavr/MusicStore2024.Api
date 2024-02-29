209a339c79fab94fda3808d88b39d71f25bb0385
diegojavr <dgarcias@outlook.com>
2024-02-29 12:47:46 -0600

    BaseResponse y GenreService

---
+++ MusicStore.Dto/Response/BaseResponsePagination.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicStore.Dto.Response
+{
+    public class BaseResponsePagination<T> : BaseResponse
+    {
+        public ICollection<T>? Data { get; set; }
+
+        public int TotalPages { get; set; }
+    }
+}
