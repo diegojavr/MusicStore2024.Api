d61cad103ac1f205813a920818f332bb40a7f9cd
diegojavr <dgarcias@outlook.com>
2024-05-13 16:52:32 -0600

    PingHealthCheck

---
+++ MusicStore.HealthCheckApi/HealthChecks/PingHealthCheck.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Net.NetworkInformation;
+
+namespace MusicStore.HealthCheckApi.HealthChecks
+{
+    public class PingHealthCheck : IHealthCheck
+    {
+        private readonly string _host;
+
+        public PingHealthCheck(string host)
+        {
+            _host=host;
+        }
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var ping = new Ping();
+
+            HealthCheckResult result;
+            try
+            {
+                var reply = await ping.SendPingAsync(_host,2000);
+                switch (reply.Status)
+                {
+                    case IPStatus.Success:
+                        result = HealthCheckResult.Healthy($"El host {_host} funciona a la perfeccion");
+                        break;
+                    case IPStatus.BadDestination:
+                        result = HealthCheckResult.Unhealthy($"El host {_host} no es accesible");
+                        break;
+                    case IPStatus.TimedOut:
+                    case IPStatus.TimeExceeded:
+                        result = HealthCheckResult.Degraded($"El host {_host} no responde rapidamente");
+                        break;
+                    default:
+                        result = HealthCheckResult.Unhealthy($"El host {_host} no es accesible");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                result = HealthCheckResult.Unhealthy(ex.Message);
+            }
+            return result;
+        }
+    }
+}
--- MusicStore.HealthCheckApi/Program.cs
+++ MusicStore.HealthCheckApi/Program.cs
@@ -2,6 +2,7 @@ using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using MusicStore.HealthCheckApi.HealthChecks;
 using MusicStore.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,10 +13,21 @@ builder.Services.AddDbContext<MusicStoreDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MusicStoreDb"));
 });
 
+
+builder.Services
+    .AddHealthChecksUI(setup =>
+    {
+        setup.SetHeaderText("Comprobacion del estado de Music Store API");
+        setup.SetEvaluationTimeInSeconds(10);
+    }).AddInMemoryStorage();
+
+
 //valida configuracion del api y de la base de datos
 builder.Services.AddHealthChecks()
     .AddCheck("self", () => HealthCheckResult.Healthy(), tags: new[] { "api" })
-    .AddDbContextCheck<MusicStoreDbContext>(tags: new[] {"database"});
+    .AddDbContextCheck<MusicStoreDbContext>(tags: new[] {"database"})
+    .AddTypeActivatedCheck<PingHealthCheck>("Firebase",HealthStatus.Degraded, tags: new[] {"api"},args: "firebase.com")
+    .AddTypeActivatedCheck<PingHealthCheck>("Azure", HealthStatus.Unhealthy, tags: new[] { "api" }, args: "azure.microsoft.com");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -37,5 +49,7 @@ app.MapHealthChecks("/health/database", new HealthCheckOptions()
     Predicate = x => x.Tags.Contains("database")
 });
 
+
+app.MapHealthChecksUI();
 app.Run();
 
